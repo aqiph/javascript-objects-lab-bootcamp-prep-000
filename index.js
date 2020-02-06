e09f943ee8bd885a1fdfd5f79fb259e8e982f3c3
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-06 21:07:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]:value})
 }
 
+//destructivelyUpdateObjectWithKeyAndValue(object, key, value)
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+
