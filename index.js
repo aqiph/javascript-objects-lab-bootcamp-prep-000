ac330be5d51ae30463d897a6923c56697d2f7be1
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-06 21:09:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ let recipes = {};
 
 //updateObjectWithKeyAndValue(object, key, value)
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object, {[key]:value})
+  return Object.assign({}, object, {[key]:value});
 }
 
 //destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  object[key] = value;
+  return object;
 }
 
